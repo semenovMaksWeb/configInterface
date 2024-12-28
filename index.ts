17eab2b060f6f5ac7134acc60708993d5f7b74b4
semenov <semenov>
2024-12-28 08:35:25 +0400

    fix ошибки

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 export * from "./src/interface/constuctor.interface"
 export type { CommandResultOperator } from "./src/interface/constuctor.interface"
+
 export * from "./src/interface/db.interface"
+export type { ConnectionDB } from "./src/interface/db.interface"
+
 export * from "./src/interface/ifs.interface"
 export type * from "./src/interface/json.interface.ts"
 export { TypeVarEnum } from "./src/interface/libs.interface"
